be441613cb511ad4e7d29df6982a27e42dd880a2
GORD-ON <gordongraff81@googlemail.com>
2026-05-09 02:17:27 +0200

    edge

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getTranslations } from "next-intl/server";
--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { type NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
