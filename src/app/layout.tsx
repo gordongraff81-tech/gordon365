6a1c7ae2b90fec639bf5d58aa2a328f5dc80a33b
GORD-ON <gordongraff81@googlemail.com>
2026-05-12 00:55:29 +0200

    sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,15 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Gordon365",
+  title: "gordon365",
+  description: "Microsoft Cloud Consulting & Modern Workplace Engineering",
+  alternates: {
+    canonical: "https://gordon365.com/de", // Deine primäre Sprache als Standard
+    languages: {
+      "de-DE": "https://gordon365.com/de",
+      "en-US": "https://gordon365.com/en",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -13,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="de">
       <body>
         {children}
       </body>
